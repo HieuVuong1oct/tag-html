073b3412aa76dc93fd137085d3a939f3703c9276
HieuVuong1oct <hieuvuong01@gmail.com>
2024-06-12 09:36:14 +0700

    fix app weather

--- js/app-weather.js
+++ js/app-weather.js
@@ -1,5 +1,6 @@
+//Lấy ra tọa độ từ tên thành phố
 $(document).ready(function () {
-  $("#getData").click(function () {
+  $("#getCoordinates").click(function () {
     var city = $("#city").val();
     var apiKey = "b475a77ad1eee9360b814683bff76d02";
     const limit = 1;
@@ -28,211 +29,93 @@ $(document).ready(function () {
 });
 
 $(document).ready(function () {
-    
-  $("#getD").click(function () {
+ 
+  $("#weatherToday").click(function () {
     var lat = $("#lat").val();
     var lon = $("#lon").val();
     var apiKey = "0322ef11d2cadcd44625773b4c6a2427";
-    $.ajax({
-      url: "https://api.openweathermap.org/data/2.5/forecast",
-      method: "GET",
-      dataType: "json",
-      data: {
-        lat: lat,
-        lon: lon,
-        appid: apiKey,
-        units: "metric",
-      },
-      success: function (response) {
-        console.log(typeof(response));
-        const r = response.list[0];
-        console.log(r.dt);
-        var options = {
-          timeZone: "Asia/Ho_Chi_Minh", // Đặt múi giờ Việt Nam
-          weekday: 'long',
-          year: "numeric",
-          month: "2-digit",
-          day: "2-digit",
-          hour: "2-digit",
-          minute: "2-digit",
-          second: "2-digit",
-          hour12: false,
-        };
-        
-        var formatter = new Intl.DateTimeFormat("vi-VN", options);
-        var time = new Date()
-        var formattedDate = formatter.format(time);
-        // var time = new Date(r.dt * 1000);
-       
-        console.log(time);
-        
-        console.log(formattedDate);
-        console.log(response);
-        console.log(r.weather);
-        console.log(response.city.name);
+    getWeather(lat, lon, apiKey, 0);
+  });
 
-        const iconCode = r.weather[0].icon;
-        const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+  $("#weatherTomorrow").click(function () {
+    var lat = $("#lat").val();
+    var lon = $("#lon").val();
+    var apiKey = "0322ef11d2cadcd44625773b4c6a2427";
+    getWeather(lat, lon, apiKey, 5);
+  });
 
-        $(".weather-icon").html(
-          `<img src="${iconUrl}" alt="weather icon"> Thời tiết `
-        );
-        $(".dt").text(formattedDate);
-        $(".name").text(response.city.name + "," + response.city.country);
-        $(".doc").text("Nhiệt độ hiện tại: " + r.main.temp + "°C");
-        $(".domax").text("Nhiệt độ cao nhất: " + r.main.temp_max + "°C");
-        $(".domin").text("Nhiệt độ thấp nhất: " + r.main.temp_min + "°C");
-        $(".doam").text("Độ ẩm: " + r.main.humidity + "%");
-        $(".tamnhin").text("Tầm nhìn: " + r.visibility + "m");
-        $(".gio").text("Tốc độ gió: " + r.wind.speed + "m/s");
-        if (r.clouds > 50) {
-          $(".may").text("Nhiều mây");
-        } else {
-          $(".may").text("Ít mây");
-        }
-      },
-      error: function (error) {
-        $("#result").text("Error: " + error);
-      },
-    });
+  $("#weatherTomorrowAfter").click(function () {
+    var lat = $("#lat").val();
+    var lon = $("#lon").val();
+    var apiKey = "0322ef11d2cadcd44625773b4c6a2427";
+    getWeather(lat, lon, apiKey, 13);
   });
 });
 
-$(document).ready(function () {
-    $("#mai").click(function () {
-      var lat = $("#lat").val();
-      var lon = $("#lon").val();
-      var apiKey = "0322ef11d2cadcd44625773b4c6a2427";
-      $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/forecast",
-        method: "GET",
-        dataType: "json",
-        data: {
-          lat: lat,
-          lon: lon,
-          appid: apiKey,
-          units: "metric",
-        },
-        success: function (response) {
-          console.log(typeof(response));
-          const r = response.list[5];
-          console.log(r.dt);
-          var options = {
-            timeZone: "Asia/Ho_Chi_Minh", // Đặt múi giờ Việt Nam
-            weekday: 'long',
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-            hour12: false,
-          };
-          var time = new Date(r.dt * 1000);
-          var formatter = new Intl.DateTimeFormat("vi-VN", options);
-          var formattedDate = formatter.format(time);
-        //   var dt = new Date(r.dt * 1000);
-          console.log(formattedDate);
-          console.log(response);
-          console.log(r.weather);
-          console.log(response.city.name);
-  
-          const iconCode = r.weather[0].icon;
-          const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
-  
-          $(".weather-icon").html(
-            `<img src="${iconUrl}" alt="weather icon"> Thời tiết `
-          );
-          $(".dt").text(formattedDate);
-          $(".name").text(response.city.name + "," + response.city.country);
-          $(".doc").text("");
-          $(".domax").text("Nhiệt độ cao nhất: " + r.main.temp_max + "°C");
-          $(".domin").text("Nhiệt độ thấp nhất: " + r.main.temp_min + "°C");
-          $(".doam").text("Độ ẩm: " + r.main.humidity + "%");
-          $(".tamnhin").text("Tầm nhìn: " + r.visibility + "m");
-          $(".gio").text("Tốc độ gió: " + r.wind.speed + "m/s");
-          if (r.clouds > 50) {
-            $(".may").text("Nhiều mây");
-          } else {
-            $(".may").text("Ít mây");
-          }
-        },
-        error: function (error) {
-          $("#result").text("Error: " + error);
-        },
-      });
-    });
+//Lấy thông tin thời tiết dựa vào tọa độ
+
+function getWeather(lat, lon, apiKey, listIndex) {
+  $.ajax({
+    url: "https://api.openweathermap.org/data/2.5/forecast",
+    method: "GET",
+    dataType: "json",
+    data: {
+      lat: lat,
+      lon: lon,
+      appid: apiKey,
+      units: "metric",
+    },
+    success: function (response) {
+      const r = response.list[listIndex];
+      var options = {
+        timeZone: "Asia/Ho_Chi_Minh", // Đặt múi giờ Việt Nam
+        weekday: "long",
+        year: "numeric",
+        month: "2-digit",
+        day: "2-digit",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit",
+        hour12: false,
+      };
+      var formatter = new Intl.DateTimeFormat("vi-VN", options);
+      if (listIndex == 0) {
+        var time = new Date();
+      } else if (listIndex == 5) {
+        var time = new Date();
+        time.setDate(time.getDate() + 1);
+      } else if (listIndex == 13) {
+        var time = new Date();
+        time.setDate(time.getDate() + 2);
+      }
+      console.log(listIndex);
+      var formattedDate = formatter.format(time);
+
+      const iconCode = r.weather[0].icon;
+      const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
+
+      $(".weather-icon").html(
+        `<img src="${iconUrl}" alt="weather icon"> Thời tiết `
+      );
+      $(".dateTime").text(formattedDate);
+      $(".name").text(response.city.name + "," + response.city.country);
+      $(".temp").text("Nhiệt độ hiện tại: " + r.main.temp + "°C");
+      $(".tempMax").text("Nhiệt độ cao nhất: " + r.main.temp_max + "°C");
+      $(".tempMin").text("Nhiệt độ thấp nhất: " + r.main.temp_min + "°C");
+      $(".humidity").text("Độ ẩm: " + r.main.humidity + "%");
+      $(".visibility").text("Tầm nhìn: " + r.visibility + "m");
+      $(".windSpeed").text("Tốc độ gió: " + r.wind.speed + "m/s");
+      if (r.clouds > 50) {
+        $(".cloud").text("Nhiều mây");
+      } else {
+        $(".cloud").text("Ít mây");
+      }
+    },
+    error: function (error) {
+      $("#result").text("Error: " + error);
+    },
   });
-
-  $(document).ready(function () {
-    $("#kia").click(function () {
-      var lat = $("#lat").val();
-      var lon = $("#lon").val();
-      var apiKey = "0322ef11d2cadcd44625773b4c6a2427";
-      $.ajax({
-        url: "https://api.openweathermap.org/data/2.5/forecast",
-        method: "GET",
-        dataType: "json",
-        data: {
-          lat: lat,
-          lon: lon,
-          appid: apiKey,
-          units: "metric",
-        },
-        success: function (response) {
-          console.log(typeof(response));
-          const r = response.list[13];
-          console.log(r.dt);
-          var options = {
-            timeZone: "Asia/Ho_Chi_Minh", // Đặt múi giờ Việt Nam
-            weekday: 'long',
-            year: "numeric",
-            month: "2-digit",
-            day: "2-digit",
-            hour: "2-digit",
-            minute: "2-digit",
-            second: "2-digit",
-            hour12: false,
-          };
-          var time = new Date(r.dt * 1000);
-          var formatter = new Intl.DateTimeFormat("vi-VN", options);
-          var formattedDate = formatter.format(time);
-        //   var dt = new Date(r.dt * 1000);
-          console.log(formattedDate);
-          console.log(response);
-          console.log(r.weather);
-          console.log(response.city.name);
-  
-          const iconCode = r.weather[0].icon;
-          const iconUrl = `http://openweathermap.org/img/wn/${iconCode}.png`;
-  
-          $(".weather-icon").html(
-            `<img src="${iconUrl}" alt="weather icon"> Thời tiết `
-          );
-          $(".dt").text(formattedDate);
-          $(".name").text(response.city.name + "," + response.city.country);
-          $(".doc").text("");
-          $(".domax").text("Nhiệt độ cao nhất: " + r.main.temp_max + "°C");
-          $(".domin").text("Nhiệt độ thấp nhất: " + r.main.temp_min + "°C");
-          $(".doam").text("Độ ẩm: " + r.main.humidity + "%");
-          $(".tamnhin").text("Tầm nhìn: " + r.visibility + "m");
-          $(".gio").text("Tốc độ gió: " + r.wind.speed + "m/s");
-          if (r.clouds > 50) {
-            $(".may").text("Nhiều mây");
-          } else {
-            $(".may").text("Ít mây");
-          }
-        },
-        error: function (error) {
-          $("#result").text("Error: " + error);
-        },
-      });
-    });
-  });
-
-
-
-
+}
 // var dt = new Date(1661878800 * 1000); // Nhân với 1000 để đổi sang mili giây
 // console.log(dt);
 // //21.0278
